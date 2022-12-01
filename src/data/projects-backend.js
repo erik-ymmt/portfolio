64d131dd443da95f4f8cdb17356e66a39beeb715
Erik Yamamoto <erik.ymmt@gmail.com>
2022-12-01 00:05:17 -0300

    feat: add TFC Project

--- src/data/projects-backend.js
+++ src/data/projects-backend.js
@@ -1,9 +1,20 @@
+import tfcImg from "../img/p_tfc-min.png";
 import blogsImg from "../img/p_be_blogs-min.png";
 import storeImg from "../img/p_be_store-min.png";
 import trybesmithImg from "../img/p_be_trybesmith-min.png";
 import rpgImg from "../img/p_be_rpg-min.png";
 
 const projectsBack = [
+  {
+    nameEN: "Trybe Futebol Clube",
+    descriptionEN: "RESTful API for a football tournament. *Frontend developed by Trybe.",
+    namePT: "Trybe Football Club",
+    descriptionPT: "API RESTful para um torneio de futebol. *Frontend desenvolvido pela Trybe.",
+    img: tfcImg,
+    techs: ["Typescript", "Express", "Sequelize", "MySQL", "Docker", "Netlify", "Railway", "Mocha", "Chai"],
+    deployLink: "https://tfc-erik.netlify.app/",
+    repositoryLink: "https://github.com/erik-ymmt/tfc-trybe-project",
+  },
   {
     nameEN: "Blogs API",
     descriptionEN: "RESTful API for a blog with CRUD operations. Documentation with Swagger. Deploy in an AWS EC2 instance",
